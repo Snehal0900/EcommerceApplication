7d72d3c4f595199b442273325101d1816f086dc8
Hp <snehal0900@gmail.com>
2025-01-18 18:18:08 +0530

    Added getters and setters lombok annotation

--- src/main/java/com/example/ecommerce/entity/Roles.java
+++ src/main/java/com/example/ecommerce/entity/Roles.java
@@ -8,9 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "roles")
